4b5ed8c226f35cfc03ef937187142bdb9b7f974e
varunvk4041 <varunvk4041@gmail.com>
2022-07-23 13:38:15 +0530

    Complete game in browser's console
    game runs well and smooth inside console
    showing scores victory/defeat message at last

--- game.js
+++ game.js
@@ -1,30 +1,68 @@
 let signs = ['Rock', 'Paper', 'Scissors']
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
-    console.log(signs[choice])
+    return signs[choice];
 }
 function playRound(playerSelection, computerSelection) {
-    let str;
+    let win_str = 'You Win!';
+    let lose_str = 'You Lose!';
+    let final_str;
+    let playerScore = computerScore = 0;
+
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
     if (playerSelection === computerSelection) {
-        str = 'Tie!';
+        final_str = 'Tie!';
     }
-    else if(playerSelection === /rock/i && computerSelection === /paper/i){
-        str ='You Lose! Paper beats Rock!';
+    else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+        final_str = win_str + " ROCK beats SCISSORS."
+        playerScore++
     }
-    else if(playerSelection === /paper/i && computerSelection === /rock/i){
-        str ='You Win! Paper beats Rock!';
+    else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') {
+        final_str = lose_str + " ROCK beats SCISSORS."
+        computerScore++
     }
-    else if(playerSelection === /rock/i && computerSelection === /scissors/i){
-        str ='You Win! Rock beats Scissors!';
+    else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        final_str = win_str + " SCISSORS beat PAPER."
+        playerScore++
     }
-    else if(playerSelection === /scissors/i && computerSelection === /rock/i){
-        str ='You Lose! Rock beats Scissors!';
+    else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+        final_str = lose_str + " SCISSORS beat PAPER."
+        computerScore++
     }
-    else if(playerSelection === /scissors/i && computerSelection === /paper/i){
-        str ='You Win! Scissors beats Paper!';
+    else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+        final_str = win_str + " PAPER beats ROCK."
+        playerScore++
     }
-    else {
-        str = 'You Lose! Paper beats Rock!'
+    else // player = rock, computer = paper 
+    {
+        final_str = lose_str + " PAPER beats ROCK."
+        computerScore++
     }
-    return str;
+    console.log(playerSelection, computerSelection)
+    return [final_str, playerScore, computerScore]
 }
+function game() {
+    let final_playerScore = 0;
+    let final_computerScore = 0;
+    let rounds = 0;
+    for (let i = 0; i < 5; i++) {
+        console.log('Round ',++rounds)
+        let playerChoice = prompt("What do you pick? Rock, Paper or Scissors?")
+        let computerChoice = getComputerChoice();
+        let results = playRound(playerChoice, computerChoice);
+        final_playerScore += results[1];
+        final_computerScore += results[2];
+        console.log(results[0])
+        console.log('Your Score = '+final_playerScore)
+        console.log('Computer\'s Score = '+final_computerScore+'\n\n')
+    }
+    if (final_computerScore > final_playerScore) {
+        console.log('You Are Defeated! Computer Won!')
+    }
+    else{
+        console.log('Victory! You have Won the game!')
+    }
+    console.log('Rounds Over, Restart to play Again!')
+}
+game()
